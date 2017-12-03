cc9d3daa90d906487699f95aaa04ef54e3b597f3
lisa_eriksson <lisa.c.eriksson@gmail.com>
2017-12-03 21:44:06 +0100

    Edit Article to associate with comments, add Comments controller and migration

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
   # get 'landing/index'
 
   root controller: :landing, action: :index
-  resources :articles
+  resources :articles do
+      resources :comments
+  end
 end
